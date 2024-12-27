a6310ba349d8db2e8dee04901c264c557dc69fb1
david <kdavidmacharia8485@gmail.com>
2024-12-27 16:22:49 +0300

    updates

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,10 +1,5 @@
-// reacts
 import { useEffect, useRef } from "react";
-
-// rrd imports
 import { Form, useFetcher } from "react-router-dom"
-
-// library imports
 import { CurrencyDollarIcon } from "@heroicons/react/24/solid"
 
 const AddBudgetForm = () => {
